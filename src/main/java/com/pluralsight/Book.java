03a4668d527e4fa03fd20bf197f2901469e2814a
AbiramiThavamani <athavamani@pgh.yearup.org>
2024-04-24 01:21:21 -0400

    library checkIn and checkOut display

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,68 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Book {
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String CheckedOutTo;
+
+    private String bookName;
+
+    public Book(int id, String isbn, String title, String name) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.CheckedOutTo = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return CheckedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        CheckedOutTo = checkedOutTo;
+    }
+
+
 }
+    
+
+
+
+
--- src/main/java/com/pluralsight/Neighborhoodlibrary.java
+++ src/main/java/com/pluralsight/Neighborhoodlibrary.java
@@ -1,4 +1,73 @@
 package com.pluralsight;
 
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Neighborhoodlibrary {
+    public static void main(String[] args) {
+        //books inventory
+        Book[] books = new Book[5];
+        books[0] = new Book(116148870, "978-0-13-601970-1", "Trust", "");
+        books[1] = new Book(116148871, "978-0-13-601971-2", "The Coming", "");
+        books[2] = new Book(116148870, "978-0-13-601972-4", "Bitter Sweet", "");
+        books[3] = new Book(116148870, "978-0-13-601973-3", "Caste", "");
+        books[4] = new Book(116148870, "978-0-13-601974-9", "IRON FLAME", "");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the value 1 to Show Available Books");
+        System.out.println("Enter the value 2 to Show Checked Out Books");
+        System.out.println("Enter the value 3 to Exit Application");
+
+        int homeMenu = scanner.nextInt();
+//        System.out.println(homeMenu);
+
+        if (homeMenu == 1) {
+            System.out.println("available books");
+        } else if (homeMenu == 2) {
+            System.out.println("show Checked Out Books");
+        } else if (homeMenu == 3) {
+            System.out.println("Exit Application");
+        } else {
+            System.out.println("invalid number");
+        }
+
+        Neighborhoodlibrary library = new Neighborhoodlibrary();
+        library.showAvailableBooks(books);
+    }
+
+   public void showAvailableBooks(Book[] books) {
+        for (int i = 0; i<books.length; i++) {
+           if (!books[i].isCheckedOut()){
+               System.out.println(books[i].getId() + "   " + books[i].getIsbn() + "   " + books[i].getTitle());
+           }
+
+        }
+   }
+
+
+
+
+
+//        // Method for checking out;
+//        public void checkOut(String name) {
+//            if (!isCheckedOut) {
+//                isCheckedOut = true;
+//                CheckedOutTo = name;
+//                System.out.println(title + "has been checked out to " + name);
+//            } else {
+//                System.out.println(title + " is already checked out ");
+//            }
+//        }
+//            // Method for checkIn;
+//            public void checkIn () {
+//                if (isCheckedOut) {
+//                    isCheckedOut = false;
+//                    System.out.println(title + " has been checked in ");
+//                } else {
+//                    System.out.println(title + " is not checked out ");
+//                }
+//            }
+//        }
+
 }
